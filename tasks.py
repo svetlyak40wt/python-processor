34762dd42571fc9771aeb14c352ec39f8f541746
Alexander Artemenko <svetlyak.40wt@gmail.com>
2015-03-19 13:42:39 +0300

    Add task for building documentation.

--- tasks.py
+++ tasks.py
@@ -3,5 +3,10 @@ from invoke import run, task
 @task
 def upload():
     run('python setup.py register')
-#    run('python setup.py sdist upload')
+    run('python setup.py sdist upload')
     run('python setup.py bdist_wheel upload')
+
+@task
+def serve_docs():
+    run('sphinx-build -b html docs dist/docs')
+    run('cd dist/docs && python -mhttp.server --bind localhost')
