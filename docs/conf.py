5860a7f884ca8e2084b3b2e136066577efd53c23
Alexander Artemenko <svetlyak.40wt@gmail.com>
2015-05-01 22:04:37 +0300

    Workaround to make docs build on read the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,20 @@ version = release = re.findall(
     open(os.path.join(os.path.dirname(__file__), '../setup.py')).read()
 )[0]
 
-import sphinx_py3doc_enhanced_theme
-html_theme = "sphinx_py3doc_enhanced_theme"
-html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if on_rtd:
+    # workaround for making build on read the docs pass
+    # see issue https://github.com/rtfd/readthedocs.org/issues/1202
+    html_theme = 'sphinx_rtd_theme'
+else:
+    import sphinx_py3doc_enhanced_theme
+    html_theme = "sphinx_py3doc_enhanced_theme"
+    html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
+    html_theme_options = {
+        'githuburl': 'https://github.com/svetlyak40wt/python-processor/'
+    }
+
 
 pygments_style = 'trac'
 templates_path = ['.']
@@ -37,6 +48,3 @@ html_sidebars = {
    '**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html'],
 }
 html_short_title = '%s-%s' % (project, version)
-html_theme_options = {
-    'githuburl': 'https://github.com/svetlyak40wt/python-processor/'
-}
